206e5d7a8bd72f7810d2aa70da56e1ee9c04cf9e
yangdezhi.eric <yangdezhi.eric@bytedance.com>
2022-08-03 16:51:08 +0800

    fix: 添加4

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ class App extends React.Component {
 	render () {
 		return (
 			<div>
-				<h1>Hello World hahaha!yddd11/////</h1>
+				<h1>Hello World hahaha!yddd11/////????</h1>
 			</div>
 		)
 	}
