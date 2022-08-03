f1aa7125d63cb37025d98c96b3fb11a895d89277
yangdezhi.eric <yangdezhi.eric@bytedance.com>
2022-08-03 16:02:38 +0800

    fix: 添加新hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'commonjs': true,
-		'es2021': true
+	"env": {
+		"browser": true,
+		"commonjs": true,
+		"es2021": true
 	},
-	'extends': [
-		'plugin:react/recommended',
-		'standard'
+	"extends": [
+		"plugin:react/recommended",
+		"standard"
 	],
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'ecmaFeatures': {
-			'jsx': true
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"ecmaFeatures": {
+			"jsx": true
 		},
-		'ecmaVersion': 'latest'
+		"ecmaVersion": "latest"
 	},
-	'plugins': [
-		'react',
-		'@typescript-eslint'
+	"plugins": [
+		"react",
+		"@typescript-eslint"
 	],
-	'settings': {
-		'react': {
-			'version': 'detect'
+	"settings": {
+		"react": {
+			"version": "detect"
 		}
 	},
-	'rules': {
-		'indent': [2, 'tab'],
-		'no-tabs': 'off'
+	"rules": {
+		"indent": [2, "tab"],
+		"no-tabs": "off"
 	}
 }
